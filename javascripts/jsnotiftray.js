52b4e7de9cd9e5fe546f2a29624e0a75629ffb62
kylehuff <code@curetheitch.com>
2015-01-10 11:26:39 -0500

    Auto build on commit

--- javascripts/jsnotiftray.js
+++ javascripts/jsnotiftray.js
@@ -97,9 +97,10 @@ var jsnotiftray = {
     // Notification box
     this.notification = document.createElement("span");
     this.notification.setAttribute("class", "jsnotiftray-vcenter jsnotiftray");
+    this.notification.css({'max-height': window.innerHeight - 2 + "px"});
     this.notification.addEventListener("click", function(e) {
       if (e.target === e.currentTarget)
-        this.hide();
+        jsnotiftray.clickHandler();
     }, false);
 
     // Notification box group
@@ -219,6 +220,10 @@ var jsnotiftray = {
     this.notification.show = this.notify;
 
     document.body.appendChild(this.notification);
+  },
+
+  clickHandler: function() {
+    jsnotiftray.hide();
   },
 
   hide: function() {
